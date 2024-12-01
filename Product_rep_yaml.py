adf34986a56d8eeadf7df431dead1b18867e59d9
GlebSvetikov <113389336+GlebSvetikov@users.noreply.github.com>
2024-12-01 14:32:09 +0300

    изменил для наследования от ProductRepository:

--- Product_rep_yaml.py
+++ Product_rep_yaml.py
@@ -1,11 +1,13 @@
 import yaml
 import json
 from decimal import Decimal
-from typing import List, Optional
+from typing import List
 from Product import Product
+from ProductRepository import ProductRepository
 
-class ProductRepYaml:
+class ProductRepYaml(ProductRepository):
     def __init__(self, filename: str):
+        super().__init__()
         self.filename = filename
         self.products = self._read_all()
 
@@ -20,39 +22,3 @@ class ProductRepYaml:
     def _write_all(self):
         with open(self.filename, 'w', encoding='utf-8') as file:
             yaml.dump([json.loads(product.to_json()) for product in self.products], file, allow_unicode=True, default_flow_style=False)
-
-    def get_by_id(self, product_id: int) -> Optional[Product]:
-        for product in self.products:
-            if product.product_id == product_id:
-                return product
-        return None
-
-    def get_k_n_short_list(self, k: int, n: int) -> List[Product]:
-        start_index = (k - 1) * n
-        end_index = start_index + n
-        return self.products[start_index:end_index]
-
-    def sort_by_field(self, field_name: str):
-        self.products.sort(key=lambda x: getattr(x, field_name))
-
-    def add_product(self, product: Product):
-        max_id = max((p.product_id for p in self.products if p.product_id is not None), default=0)
-        product.product_id = max_id + 1
-        self.products.append(product)
-        self._write_all()
-
-    def replace_product_by_id(self, product_id: int, new_product: Product):
-        for i, product in enumerate(self.products):
-            if product.product_id == product_id:
-                self.products[i] = new_product
-                new_product.product_id = product_id
-                self._write_all()
-                return
-        raise ValueError(f"Продукт с ID {product_id} не найден.")
-
-    def delete_product_by_id(self, product_id: int):
-        self.products = [product for product in self.products if product.product_id != product_id]
-        self._write_all()
-
-    def get_count(self) -> int:
-        return len(self.products)
